423165f09ae84b2f50a79dda8e383ba330d7e60e
Jelle Ceulemans <info@jelleceulemans.be>
2019-11-17 15:08:03 +0100

    Vue
    Completed section 10

---
+++ VueJS2/Section9/Assignment8/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+
+                <!--<app-blue>
+                    <h1>Blue component!</h1>
+                    <p>Is this blue component you like the most ?</p>
+                </app-blue>
+                <app-green>
+                    <h1>Green component!</h1>
+                    <p>Is this green component you like the most ?</p>
+                </app-green>
+                <app-red>
+                    <h1>Red component!</h1>
+                    <p>Is this red component you like the most ?</p>
+                </app-red>-->
+
+                <component :is="selectedComponent">
+                    <h2>This is the Selected Component Content</h2>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: function () {
+            return {
+                selectedComponent: ''
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
--- VueJS2/Section9/Example1/src/App.vue
+++ VueJS2/Section9/Example1/src/App.vue
@@ -2,16 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+                <button @click="selectComponent = 'appQuote'">Quote</button>
+                <button @click="selectComponent = 'appAuthor'">Author</button>
+                <button @click="selectComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{selectComponent}}</p>
+<!--               <app-quote>-->
+<!--                   <h2 slot="title">{{quoteTitle}}</h2>-->
+<!--                   <p>A wonderful Quote</p>-->
+<!--               </app-quote>-->
+                <keep-alive>
+                    <component :is="selectComponent">
+                        <p>Default Content </p>
+                    </component>
+                </keep-alive>
+
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
-        
+        data: function() {
+            return {
+                quoteTitle: 'The Quote',
+                selectComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
     }
 </script>
 
