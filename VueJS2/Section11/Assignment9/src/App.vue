33305692cbd716ea56f28e1c38c50365aaa9b3af
Jelle Ceulemans <info@jelleceulemans.be>
2019-11-20 15:15:24 +0100

    Vue
    Finished section 11

--- VueJS2/Section11/Assignment9/src/App.vue
+++ VueJS2/Section11/Assignment9/src/App.vue
@@ -1,8 +1,35 @@
 <template>
     <div class="container">
         <form>
-            <div class="row">
+            <div class="row" v-show="!isSubmitted">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+
+<!--                    <div class="form-group">-->
+<!--                        <label for="firstName">Full Name</label>-->
+<!--                        <input type="text" id="firstName" v-model="signup.firstName" class="form-control">-->
+<!--                    </div>-->
+<!--                    <div class="form-group">-->
+<!--                        <label for="lastName">Full Name</label>-->
+<!--                        <input type="text" id="lastName" v-model="signup.lastName" class="form-control">-->
+<!--                    </div>-->
+                    <app-full-name v-model="signup.fullName"></app-full-name>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" v-model="signup.email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" v-model="signup.password" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <p>Store data ?</p>
+                        <label for="yes">Yes</label>
+                        <input type="radio" id="yes" v-model="signup.storeData" value="Yes" class="radio">
+                        <label for="no">No</label>
+                        <input type="radio" id="no" v-model="signup.storeData" value="No" class="radio">
+                    </div>
+
+                    <button class="btn btn-info" type="submit" @click.prevent="onSubmit">Sumit</button>
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
@@ -21,17 +48,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-show="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{signup.fullName}}</p>
+                        <p>Mail: {{signup.email}}</p>
+                        <p>Password: {{signup.password}}</p>
+                        <p>Store in Database?: {{signup.storeData}}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +67,27 @@
 </template>
 
 <script>
+    import FullName from './components/FullName.vue';
     export default {
+        data() {
+            return {
+                signup: {
+                    fullName: 'Jelle Ceulemans',
+                    email: '',
+                    password: '',
+                    storeData: 'Yes'
+                },
+                isSubmitted: false
+            }
+        },
+        methods: {
+            onSubmit() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            appFullName: FullName
+        }
     }
 </script>
 
---
+++ VueJS2/Section11/Assignment9/src/components/FullName.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="firstName">Full Name</label>
+            <input type="text" id="firstName" :value="firstName" class="form-control" @input="nameChanged(true, $event)">
+        </div>
+        <div class="form-group">
+            <label for="lastName">Full Name</label>
+            <input type="text" id="lastName" :value="lastName" class="form-control" @input="nameChanged(false, $event)">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        methods: {
+            nameChanged(isFirst, event) {
+                let name = '';
+                if (isFirst) {
+                    name = event.target.value + ' ' +this.lastName;
+                } else {
+                    name = this.firstName + ' ' + event.target.value;
+                }
+                this.value = name;
+                this.$emit('input', name);
+            }
+        },
+        computed: {
+            firstName() {
+                return this.value.split(" ")[0];
+            },
+            lastName() {
+                return this.value.split(" ")[1];
+            }
+        }
+
+    }
+</script>
+
+<style scoped></style>
