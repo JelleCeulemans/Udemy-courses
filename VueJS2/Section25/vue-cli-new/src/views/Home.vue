bd5c9a604345d12fb4279e209cf53c04bfcc4cb6
Jelle Ceulemans <info@jelleceulemans.be>
2019-11-29 18:08:17 +0100

    Vue Finished course

---
+++ VueJS2/Section25/vue-cli-new/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <footer>
+      <small>blalalalalala</small>
+    </footer>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  footer {
+    small {
+      color: red;
+    }
+  }
+</style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
